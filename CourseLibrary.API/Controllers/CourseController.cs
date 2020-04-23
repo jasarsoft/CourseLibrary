59cccf7d5c7dc28eb7590182289cca794bce720d
Edin Jašarević <edinjasar14@gmail.com>
2020-04-23 12:40:56 +0200

    validating input when updating a resource with patch

--- CourseLibrary.API/Controllers/CourseController.cs
+++ CourseLibrary.API/Controllers/CourseController.cs
@@ -120,7 +120,11 @@ namespace CourseLibrary.API.Controllers
 
             var courseToPatch = _mapper.Map<CourseForUpdateDto>(courseForAuthorFromRepo);
             // add validation
-            patchDocument.ApplyTo(courseToPatch);
+            patchDocument.ApplyTo(courseToPatch, ModelState);
+            if (!TryValidateModel(courseToPatch))
+            {
+                return ValidationProblem(ModelState);
+            }
 
             _mapper.Map(courseToPatch, courseForAuthorFromRepo);
 
